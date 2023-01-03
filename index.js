94e3ebcd7aeb275b22f0d480d09416fb8b13e2f6
Kunal Titare <114751317+Kunal1309@users.noreply.github.com>
2023-01-03 08:56:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+               <img src="${document.querySelector('#newtask input').value}" alt="check address">
+                <button class="delete">
+                    Remove
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
+}
+
